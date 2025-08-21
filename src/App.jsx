2fd51835170bb50cef87bdbcfe0756a110641cc7
RaihanAmiree <raihan.a.amiree@gmail.com>
2025-08-21 14:59:48 +0600

    Added working Update functionality in Todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
+import Update from "./Components/Update";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="relative h-full pb-32">
+      
       <div className="w-100vw h-[clamp(100px,_10vw,_150px)] bg-gray-700 text-center font-[700] flex items-center justify-center text-[clamp(30px,_4vw,_50px)] text-white">
         To Do Application
       </div>
       <Form></Form>
-    </>
+      {/* <Update></Update> */}
+    </div>
   );
 }
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,6 +6,7 @@ import {
   push,
   onValue,
   remove,
+  update,
 } from "firebase/database";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import deleteIcon from "../assets/Delete.jpg";
@@ -14,6 +15,9 @@ import editIcon from "../assets/Edit.png";
 const Form = () => {
   const [Todo, setTodo] = useState("");
   const [TodoList, setTodoList] = useState([]);
+  const [showEdit, setShowEdit] = useState(false);
+  const [edit, setEdit] = useState("");
+  const [editId, setEditId] = useState(null);
 
   const handleChange = (e) => {
     setTodo(e.target.value);
@@ -53,6 +57,43 @@ const Form = () => {
     remove(TodoRef);
   }
 
+  function handleEdit(value, id) {
+    setShowEdit(true);
+    setEdit(value);
+    setEditId(id);
+  }
+
+  console.log(edit);
+
+  function handleUpdate(e, id) {
+    e.preventDefault();
+    setShowEdit(false);
+    const db = getDatabase();
+    update(ref(db, "Todo/" + editId), {
+      TodoName: edit,
+    }).then(() => {
+      notifyUpdate();
+    });
+  }
+
+  const notifyUpdate = () =>
+    toast.success("Your Task has been successfully updated", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+      style: {
+        width: "clamp(260px, 75vw, 380px)",
+        padding: "clamp(12px, 3vw, 20px)",
+        fontSize: "clamp(14px, 2.5vw, 18px)",
+      },
+    });
+
   const notify = () =>
     Todo == ""
       ? toast.error("Please enter your task", {
@@ -90,6 +131,34 @@ const Form = () => {
 
   return (
     <>
+      {showEdit && (
+        <>
+          <div className="fixed inset-0 z-10 flex items-center justify-center">
+            <div className="absolute inset-0 bg-[#000000c0]"></div>
+            <div className="bg-gray-700  p-[clamp(16px,_4vw,_32px)] rounded-2xl shadow-2xl w-[clamp(320px,_80vw,_700px)] mx-auto mt-[clamp(40px,_10vh,_80px)] z-20">
+              <h1 className="text-[clamp(24px,_6vw,_48px)] font-extrabold text-white mb-[clamp(16px,_4vw,_32px)] text-center">
+                Update Your Task
+              </h1>
+              <form className="mb-[clamp(24px,_6vw,_48px)]">
+                <input
+                  type="text"
+                  value={edit}
+                  placeholder="Update your Task"
+                  className="w-full font-[600] p-[clamp(12px,_3vw,_16px)] border border-gray-500 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400 text-[clamp(16px,_3vw,_20px)]  bg-white text-gray-800"
+                  onChange={(e) => setEdit(e.target.value)}
+                />
+              </form>
+              <button
+                className="w-[clamp(150px,_40vw,_250px)] block mx-auto bg-blue-600 hover:bg-blue-700 text-[clamp(18px,_4vw,_24px)] text-white font-bold py-[clamp(12px,_3vw,_16px)] px-[clamp(24px,_6vw,_32px)] rounded-2xl shadow-md transition duration-300 ease-in-out hover:scale-105 focus:outline-none cursor-pointer"
+                onClick={handleUpdate}
+              >
+                Update
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+
       <div className="bg-gray-700 p-[clamp(16px,_4vw,_32px)] rounded-2xl shadow-2xl w-[clamp(320px,_80vw,_700px)] mx-auto mt-[clamp(40px,_10vh,_80px)]">
         <h1 className="text-[clamp(24px,_6vw,_48px)] font-extrabold text-white mb-[clamp(16px,_4vw,_32px)] text-center">
           Enter Your Task
@@ -139,7 +208,14 @@ const Form = () => {
                 </li>
                 <div className="flex gap-[clamp(8px,_2vw,_12px)]">
                   <button className="w-[clamp(32px,_8vw,_40px)] h-[clamp(32px,_8vw,_40px)] rounded-full bg-transparent border-[2.5px] border-gray-400 flex items-center justify-center cursor-pointer hover:bg-gray-800">
-                    <img src={editIcon} alt="" className="w-5 h-auto" />
+                    <img
+                      src={editIcon}
+                      alt=""
+                      className="w-5 h-auto"
+                      onClick={() => {
+                        handleEdit(item.value.TodoName, item.id);
+                      }}
+                    />
                   </button>
                   <button
                     className="w-[clamp(32px,_8vw,_40px)] h-[clamp(32px,_8vw,_40px)] rounded-full bg-transparent border-[2.5px] border-red-400 flex items-center justify-center hover:bg-red-800"
